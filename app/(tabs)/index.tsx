ccbe7067d494762069ddd43c758d04073000f3d3
IsmailYucelOlmez <ismailyucelolmez514@gmail.com>
2025-02-11 02:33:10 +0300

    Assets Added and Search Component Design Implemented

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,102 @@
-import { Image, StyleSheet, Platform, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { Image, StyleSheet, Platform, Text, View, TextInput, Touchable, TouchableOpacity, FlatList } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
+
+  const locations = [
+    {
+      id: 1,
+      name: 'London',
+      country: 'United Kingdom'
+    },
+    {
+      id: 2,
+      name: 'New York',
+      country: 'United States'
+    },
+    {
+      id: 3,
+      name: 'Paris',
+      country: 'France'
+    },
+    {
+      id: 4,
+      name: 'Tokyo',
+      country: 'Japan'
+    }
+  ]
+  
+  const [City,setCity]=useState("");
+  const [showInput, setShowInput] = useState(false);
+  const [location, setLocation] = useState(locations);
+
+  const toggleInput = () => {
+    setShowInput((prev)=>!prev);
+  }
+
+  const searchCity = () => {
+    
+    ;
+  }
+
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome! React Native</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <Text className='font-semibold text-2xl'>La grande aquila e morta</Text>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View className='flex-1 relative'>
+      <StatusBar style='light' />
+
+      <Image source={require('../../assets/images/bg.png')} className='w-full h-full absolute' />
+
+      <SafeAreaView className='flex'>
+        <View>
+          <View className={`m-2 ${showInput ? 'bg-white ':''} rounded-2xl flex flex-row justify-end items-center`}>
+            {showInput && (
+              <TextInput 
+              value={City} 
+              onChangeText={setCity} 
+              placeholder='Search a City' 
+              className=' flex-1 h-10 text-xs rounded-2xl px-2 focus:outline-none text-black  ' 
+              textAlignVertical='center'                                     
+              />
+            )}
+            <TouchableOpacity className='bg-white h-12 w-12 rounded-2xl flex justify-center items-center' onPress={() => toggleInput()}>
+              <AntDesign name="search1" size={24} color="black" />
+            </TouchableOpacity>
+          
+          </View>
+
+          {showInput && location.length>0 ? (
+
+            <FlatList
+              className='px-2'
+              showsHorizontalScrollIndicator={false}
+              data={location}
+              keyExtractor={(item) => item.id.toString()}
+              renderItem={({ item }) => (
+                <View className='bg-white rounded-xl p-2 border-b border-black flex justify-between items-center flex-row'>
+                  <View className='flex flex-row items-center gap-2'>
+                    <FontAwesome name="map-marker" size={18} color="black" />
+                    <Text className='text-lg'>{item.name}</Text>
+                  </View>
+                  
+                  <Text className='text-sm'>{item.country}</Text>
+                </View>
+              )}/> 
+          ) : (
+            <View className='flex-1 flex justify-center items-center'>
+              <Text className='text-white text-lg'>No City Found</Text>
+            </View>            
+          )}           
+        
+        </View>
+      </SafeAreaView>
+      
+      
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
